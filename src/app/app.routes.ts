2b9a698126298a6a26f58b68f5ded18970c996ef
Lisa <lisa.auger2@gmail.com>
2024-11-20 12:13:12 +0100

    feat: Add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'home', loadComponent: () => import('./home/home.component').then(c => c.HomeComponent)
+    },
+    {
+        path: 'game', loadComponent: () => import('./game/game.component').then(c => c.GameComponent)
+    },
+    {
+        path: '**', redirectTo: 'home'
+    }
+    
+];
