411dd47ddd2d142239f6b4b4cd13a3cd10bd8574
Lisa <lisa.auger2@gmail.com>
2024-11-26 16:52:31 +0100

    feat: Add players from HistoryState to HomeComponent

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,12 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { afterNextRender, Component, inject } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatCardModule} from '@angular/material/card';
+import { select, Store } from '@ngxs/store';
+import { HistoryState } from '../states/history.state';
+import { GetPlayers } from '../states/history.actions';
 
 @Component({
   selector: 'app-home',
@@ -19,14 +22,17 @@ import {MatCardModule} from '@angular/material/card';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-  constructor(
-    private router: Router
-  ) {}
+
+  private store = inject(Store);
+  private router = inject(Router);
+
+  constructor() {
+    afterNextRender(() => this.store.dispatch(new GetPlayers()))
+  }
+
+  players = select(HistoryState.getPlayers);
 
   onPlay() {
     this.router.navigateByUrl('/game');
   }
-
-
-
 }
