c0ad67a2ebb30ce1c3326cd18e4a5c5cb59a2621
Lisa <lisa.auger2@gmail.com>
2024-11-26 16:47:37 +0100

    feat: Add getPlayers() to GameService

--- src/app/services/game.service.ts
+++ src/app/services/game.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Player } from '../models/player.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GameService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getPlayers(): Observable<Player[]> {
+    return this.http.get<Player[]>('https://localhost/api/v1/docs/players)');
+  }
 }
