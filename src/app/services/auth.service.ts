74df83fa01418694213bb2043d4cfac3833d3315
Lisa <lisa.auger2@gmail.com>
2025-01-20 14:17:41 +0100

    refacto: Simplify AuthService

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -4,14 +4,8 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class AuthService {
-  token: string | null = null;
-
-  login(): void {
-    this.token = 'MySecret2024!';
-  }
-
+  
   getToken(): string | null {
-    this.login();
-    return this.token;
+    return 'MySecret2024!';
   }
 }
